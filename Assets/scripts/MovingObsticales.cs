3fa5f9e60c368813a0748e60804c10e1f31d23c7
Drinu89 <32704506+Drinu89@users.noreply.github.com>
2017-12-10 11:02:28 +0100

    Unity Commit 6

---
+++ Assets/scripts/MovingObsticales.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingObsticales : MonoBehaviour {
+
+    public static int movespeed = 1;
+    public Vector3 userDirection = Vector3.right;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.Translate(userDirection * movespeed * Time.deltaTime); 
+	}
+
+    void OnCollisionEnter2D(Collision2D coll)
+    {
+        if (coll.gameObject.name == "TopCollider")
+        {
+            movespeed = -1; 
+        }
+        else if (coll.gameObject.name == "BottomCollider")
+        {
+            movespeed = 1;
+        }
+    }
+}
